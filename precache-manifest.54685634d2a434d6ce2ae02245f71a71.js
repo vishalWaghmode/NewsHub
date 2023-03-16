7e4499d899faf25fc2c077d2e5fb62c82da1be8b
vishal <vishalwaghmode33284@gmail.com>
2023-03-16 22:25:25 +0530

    Updates

---
+++ precache-manifest.54685634d2a434d6ce2ae02245f71a71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d40168888e59f1b5c8fc",
+    "url": "/NewsHub/static/css/main.33b853fd.chunk.css"
+  },
+  {
+    "revision": "d40168888e59f1b5c8fc",
+    "url": "/NewsHub/static/js/main.d4016888.chunk.js"
+  },
+  {
+    "revision": "b965d220e0d6421fd4e0",
+    "url": "/NewsHub/static/js/1.b965d220.chunk.js"
+  },
+  {
+    "revision": "d7dace4b32c1bb39dcf7",
+    "url": "/NewsHub/static/js/2.d7dace4b.chunk.js"
+  },
+  {
+    "revision": "fa0359e69d9b379306ed",
+    "url": "/NewsHub/static/js/runtime~main.fa0359e6.js"
+  },
+  {
+    "revision": "7cd1f3cd481e80e3e4e43697f47b9f83",
+    "url": "/NewsHub/static/media/loading.7cd1f3cd.gif"
+  },
+  {
+    "revision": "2bcdd37ce1accdba6eefa95e756976fe",
+    "url": "/NewsHub/index.html"
+  }
+];
